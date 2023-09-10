5cf42ca583d103601ab85fe89f1e23a8b6541008
Hoàng Văn Giỏi <gioitube2k2@gmail.com>
2023-09-10 17:32:47 +0700

    first commit

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -o errexit
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate django_celery_beat
+celery -A core beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
