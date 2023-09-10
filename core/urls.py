0e249b2ea9daa8d66dfa14295b7694d577952e09
Hoàng Văn Giỏi <gioitube2k2@gmail.com>
2023-09-10 17:45:50 +0700

    first commit

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^.*$', RedirectView.as_view(url='/admin/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
